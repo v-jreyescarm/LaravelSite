cb3dfe04cc27f80d7a7af3f7dd0009f4603cc90f
unknown <v-jreyescarm@microsoft.com>
2022-03-30 18:42:50 +0000

    A tillte change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                                A little change
                             </a>
                         </div>
                     </div>
